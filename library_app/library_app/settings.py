0675ceb55ec4415e84c9e12bb865727b9b95a3ce
Haikal Fikri <haikaljr1245@gmail.com>
2024-02-07 20:06:11 +0700

    Database setup

--- library_app/library_app/settings.py
+++ library_app/library_app/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "library_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "PORT": 5432,
+        "HOST": "localhost",
     }
 }
 
